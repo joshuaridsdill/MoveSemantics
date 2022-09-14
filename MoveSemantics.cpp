edae5ab38e43a25d235cf3fd2774b3f3fa30bb0e
Joshua Ridsdill <joshuaridsdill@outlook.com>
2022-09-14 16:46:08 +0100

    Update!

--- MoveSemantics.cpp
+++ MoveSemantics.cpp
@@ -3,39 +3,10 @@
 
 #include <iostream>
 
-class Entity
-{
-public:
-    Entity(std::string&& name) noexcept
-    {
-        m_Name = name;
-    }
-
-    Entity(Entity& e) noexcept
-    {
-        m_Name = new char[sizeof(e.m_Name)];
-        std::cout << "Copied!\n";
-    }
-
-    Entity(Entity&& e) noexcept
-    {
-        m_Name = e.m_Name;
-        e.m_Name = "";
-        std::cout << "Moved!\n";
-    }
-
-    ~Entity()
-    {
-        std::cout << "Destroyed!\n";
-    }
-
-private:
-    std::string m_Name;
-};
-
 class String
 {
 public:
+    String() = default;
 
     String(const char* str)
     {
@@ -43,7 +14,7 @@ public:
         m_Data = new char[m_Size + 1];
         //memcpy_s(&m_Data, m_Size, str, m_Size);
         memcpy(m_Data, str, m_Size);
-        *(m_Data + m_Size) = 0;
+        *(m_Data + m_Size) = '\0';
         std::cout << "Created!\n";
     }
 
@@ -54,7 +25,7 @@ public:
         memcpy_s(&m_Data, m_Size, str.m_Data, m_Size);
         std::cout << "Copied!\n";
     }
-    
+
     String(String&& str) noexcept
     {
         m_Size = str.m_Size;
@@ -66,9 +37,33 @@ public:
         std::cout << "Moved!\n";
     }
 
-    ~String()
+    String& operator=(const String& str)
     {
-        //delete m_Data;
+        if (this != &str)
+        {
+            delete[] m_Data;
+
+            m_Size = str.m_Size;
+            m_Data = new char[m_Size+1];
+            memcpy(m_Data, str.m_Data, m_Size+1);
+        }
+        return *this;
+    }
+
+    String& operator=(String&& str) noexcept
+    {
+        if (this != &str)
+        {
+            delete[] m_Data;
+
+            m_Data = str.m_Data;
+            m_Size = str.m_Size;
+
+            str.m_Data = nullptr;
+            str.m_Size = 0;
+        }
+
+        return *this;
     }
 
     friend std::ostream& operator<<(std::ostream& os, String& str);
@@ -94,6 +89,47 @@ private:
     size_t m_Size;
 };
 
+class Entity
+{
+public:
+    Entity()
+    {
+        std::cout << "Created! f\n";
+        m_Name = "Hey";
+    }
+
+    Entity(String&& name) noexcept
+        :
+        m_Name(std::move(name))
+    {
+        std::cout << "Created! h\n";
+    }
+
+    Entity(const Entity& e) noexcept
+        :
+        m_Name(e.m_Name)
+    {
+        std::cout << "Copied! d\n";
+    }
+
+    Entity(Entity&& e) noexcept
+    {
+        m_Name = e.m_Name;
+        e.m_Name = nullptr;
+        std::cout << "Moved!\n";
+    }
+
+    ~Entity()
+    {
+        std::cout << "Destroyed!\n";
+    }
+
+private:
+    String m_Name;
+};
+
+
+
 std::ostream& operator<<(std::ostream& os, String& str)
 {
     os << str.m_Data;
@@ -103,16 +139,18 @@ std::ostream& operator<<(std::ostream& os, String& str)
 
 int main()
 {
-    String str = "Hello, World!";
-    String str2 = "Goodbye, World!";
-    //String str3(std::move(str));
+    String str("Hello, World!");
+    String str2(str);
+
+    //str = str2;
 
     std::cout << str << '\n';
     str.Print();
     std::cout << str.Size() << '\n';
 
-    //Entity e("Entity1");
-    //Entity e2(e);   
+    Entity e;
+    //Entity e2(e);
+    std::string;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
